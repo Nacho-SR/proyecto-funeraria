56b807e491a67f3158a41468b0619be5058a8b06
GMA88 <105989803+GMA88@users.noreply.github.com>
2026-05-20 13:11:33 -0600

    Nuevo pago controller

--- backend/src/modules/clientes/clientes.controller.js
+++ backend/src/modules/clientes/clientes.controller.js
@@ -1,2 +1,12 @@
 import nuevoPagoSchema from  'backend/src/modules/pagos/pagos.schema.js'
-import pago from  'backend/src/modules/pagos/pagos.schema.js'
+import pagoSchema from  'backend/src/modules/pagos/pagos.schema.js'
+import { ClientesService } from './clientes.service.js'
+import { ApiError } from '../../shared/utils/apiError.js'
+
+const service = new ClientesService()
+
+export const nuevoPago = async (req, res) => {
+    const doc = nuevoPagoSchema.parse(req.body)
+    const created = await service.crearNuevoPago(doc)
+    res.status(201).json(created)
+}
