075a4a78fba13e5cbbf23ba611e077d45f158923
GMA88 <105989803+GMA88@users.noreply.github.com>
2026-05-20 12:20:53 -0600

    Pagos clientes

---
+++ backend/src/modules/clientes/clientes.repo.js
@@ -0,0 +1,21 @@
+import { db } from '../../config/firebase.js'
+
+export class ClientesRepository {
+
+
+  async findBeneficiarioByName(nombre) {
+    console.log('Buscando beneficiario por nombre:', nombre)
+    const benef = await db.collection("beneficiarios").where('nombre', '==', nombre).limit(1).get()
+    if (benef.empty) return null
+    return { id: benef.docs[0].id, ...benef.docs[0].data() }
+  }
+
+  async crearBeneficiario(data) {
+    const beneficiario = await db.collection("beneficiarios").doc()
+    await beneficiario.set(data)
+    return { id: beneficiario.id, ...data }
+  }
+
+
+
+}
